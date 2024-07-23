021e01954d33bf38aca4c6ae9c98f7b45cae3f2f
gk258007 <gk258007@gmail.com>
2024-07-23 13:18:00 +0530

    added kubernetes config files

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
-FROM alpine:latest
+FROM ubuntu:20.04
 
+# install cowsay, and move the "default.cow" out of the way so we can overwrite it with "docker.cow"
+RUN apt-get update && apt-get install -y cowsay fortune fortunes-debian-hints fortunes-off netcat --no-install-recommends && rm -rf /var/lib/apt/lists/* 
 
-RUN apk update && \
-    apk add fortune cowsay openbsd-netcat
+ENV PATH $PATH:/usr/games
+COPY wisecow.sh /usr/games
 
-COPY serve_wisdom.sh /usr/local/bin/serve_wisdom.sh
-
-RUN chmod +x /usr/local/bin/serve_wisdom.sh
-
-EXPOSE 4499
-
-ENTRYPOINT ["/usr/local/bin/serve_wisdom.sh"]
+CMD ["bash","/usr/games/wisecow.sh"]
--- wisecow.sh
+++ wisecow.sh
@@ -7,41 +7,40 @@ rm -f $RSPFILE
 mkfifo $RSPFILE
 
 get_api() {
-	read line
-	echo $line
+    read line
+    echo $line
 }
 
 handleRequest() {
     # 1) Process the request
-	get_api
-	mod=`fortune`
+    get_api
+    mod=$(fortune)
 
 cat <<EOF > $RSPFILE
 HTTP/1.1 200
 
 
-<pre>`cowsay $mod`</pre>
+<pre>$(cowsay "$mod")</pre>
 EOF
 }
 
 prerequisites() {
-	command -v cowsay >/dev/null 2>&1 &&
-	command -v fortune >/dev/null 2>&1 || 
-		{ 
-			echo "Install prerequisites."
-			exit 1
-		}
+    command -v cowsay >/dev/null 2>&1 &&
+    command -v fortune >/dev/null 2>&1 || 
+        { 
+            echo "Install prerequisites."
+            exit 1
+        }
 }
 
 main() {
-	prerequisites
-	echo "Wisdom served on port=$SRVPORT..."
+    prerequisites
+    echo "Wisdom served on port=$SRVPORT..."
 
-	while [ 1 ]; do
-		cat $RSPFILE | nc -l $SRVPORT | handleRequest
-		# cat $RSPFILE | nc -lN $SRVPORT | handleRequest
-		sleep 0.01
-	done
+    while true; do
+        cat $RSPFILE | nc -l $SRVPORT | handleRequest
+        sleep 0.01
+    done
 }
 
 main
